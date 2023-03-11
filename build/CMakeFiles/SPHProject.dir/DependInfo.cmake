cdda077bf41e42b648e3a2d17f653f20c3fa5fd1
Zachary Doucet <zacharydoucet01@hotmail.com>
2023-03-11 10:37:40 -0500

    Setup repository

---
+++ build/CMakeFiles/SPHProject.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/lovelace/Documents/McGill/sem6/COMP559/project/src/main.cpp" "CMakeFiles/SPHProject.dir/src/main.cpp.o" "gcc" "CMakeFiles/SPHProject.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/usr/local/lib/glfw-3.3.8/debug/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/SPHProject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SPHProject.dir/src/main.cpp.o"
+  "CMakeFiles/SPHProject.dir/src/main.cpp.o.d"
+  "SPHProject"
+  "SPHProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SPHProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
